e94b2f19524a7d1369a2989cc0da23b98650c481
alexismarlene <40073150+alexismarlene@users.noreply.github.com>
2022-05-31 21:33:34 -0400

    Update peones.py
    excelente manejo con rodajas

--- peones.py
+++ peones.py
@@ -1,3 +1,5 @@
+#excelente manejo de rodajas
+
 tab = [["vacio" for i in range(8)] for i in range(6)]
 tab[1:1] = [["peon" for i in range(8)] for i in range(1)]
 tab[6:1] = [["peon" for i in range(8)] for i in range(1)]  
